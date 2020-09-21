dfe408968335b336f08387f243dd01f288e05186
AnonymouX47 <feyidab01@gmail.com>
2020-09-21 13:42:49 +0100

    Added function to read stock data

--- project.py
+++ project.py
@@ -1,2 +1,28 @@
+"""PYTHON IFE RETAIL MARKET PROJECT"""
 
+def read_stock(filepath):
+    """Reads stock data from a csv file
+    returns a list of dicts containing the stock data
+
+    Note: Each line of the csv file is expected to be in the form:
+    "'item name', 'quantity', 'unit price'\\n"
+    """
+
+    stock_file = open(filepath, 'r')
+    stock = []  # List to contain stock data
+
+    for line in stock_file:
+        line = line.strip('\n').split(', ')
+        row = []
+        for cell in line:
+            row.append(cell.strip("'"))
+        row[1:] = map(int, row[1:])
+        stock.append({"item": row[0], "quan": row[1], "price": row[2]})
+    stock_file.close()
+
+    # stock = [ {"item": row[0], "quan": int(row[1]), "price": int(row[2])} for row in
+    #         [ [cell.strip("'") for cell in line.strip().split(', ')]
+    #         for line in open(filepath, 'r')] ]
+
+    return stock
 
