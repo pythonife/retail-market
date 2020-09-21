5fc1d6b3e72ca70d8f5f3260afc3a9577d22f416
AnonymouX47 <feyidab01@gmail.com>
2020-09-21 15:18:49 +0100

    Added function to change details of items in stock Added function to display available items Changed dictionary key for item name

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 """ PYTHON IFE RETAIL MARKET PROJECT """
 
+from utils import *
+
 def read_stock(filepath):
     """ Reads stock data from a csv file
     returns a list of dicts containing the stock data
@@ -17,12 +19,35 @@ def read_stock(filepath):
         for cell in line:
             row.append(cell.strip("'"))
         row[1:] = map(int, row[1:])
-        stock.append({"item": row[0], "quantity": row[1], "price": row[2]})
+        stock.append({"name": row[0], "quantity": row[1], "price": row[2]})
     stock_file.close()
 
-    stock = [ {"item": row[0], "quantity": int(row[1]), "price": int(row[2])}
-            for row in [ [cell.strip("'") for cell in line.strip().split(', ')]
-            for line in open(filepath, 'r')] ]
+    # stock = [ {"name": row[0], "quantity": int(row[1]), "price": int(row[2])}
+    #         for row in [ [cell.strip("'") for cell in line.strip().split(', ')]
+    #         for line in open(filepath, 'r')] ]
 
     return stock
 
+
+def display_items():
+    """ Display available items with their unit prices """
+
+    print('|', "ID".ljust(2), '||', "Item".ljust(30), '||', "Unit Price (#)".rjust(14), '|')
+    print('='*54)
+    for item_no, item in enumerate(stock, 1):
+        name, quantity, price = item.values()
+        if quantity > 0:
+            print('|', str(item_no).ljust(2),
+                    '||', name.ljust(30),
+                    '||', str(price).rjust(14), '|')
+    print()
+
+
+# stock = read_stock("data.csv")
+
+# display_items()
+
+# change_detail(stock, "price")
+# change_detail(stock, "quantity")
+
+# display_items()
--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
-)
+""" Utility functions for the Retail Market Project """
+
+def change_detail(stock, detail):
+    """ Function for admin to change ptice of items """
+
+    max_id = len(stock)
+    progress = 'y'
+    while progress == 'y':
+        try:
+            item = input("Enter item ID: ")
+            if detail != "name":
+                item = int(item)
+            if item < 1 or item > max_id:
+                print("Invalid Item ID")
+                continue
+            value = int(input("Enter new %s: " % detail))
+        except ValueError:
+            print("Invalid input for '%s'!!" % detail)
+            continue
+        else:
+            stock[item - 1][detail] = value
+            print("%s of '%s' successfully changed" % (detail, stock[item - 1]["name"]))
+        finally:
+            progress = input(
+                    "Do you want to change the %s of another item (y/n)? "
+                    % detail).lower()
+    print()
+
