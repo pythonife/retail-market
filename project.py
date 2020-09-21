a615909eb8a85f56ec893e9e1704ed505df392d6
AnonymouX47 <feyidab01@gmail.com>
2020-09-21 14:08:26 +0100

    Changed dictionary key for item quantity

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
-"""PYTHON IFE RETAIL MARKET PROJECT"""
+""" PYTHON IFE RETAIL MARKET PROJECT """
 
 def read_stock(filepath):
-    """Reads stock data from a csv file
+    """ Reads stock data from a csv file
     returns a list of dicts containing the stock data
 
     Note: Each line of the csv file is expected to be in the form:
@@ -17,12 +17,12 @@ def read_stock(filepath):
         for cell in line:
             row.append(cell.strip("'"))
         row[1:] = map(int, row[1:])
-        stock.append({"item": row[0], "quan": row[1], "price": row[2]})
+        stock.append({"item": row[0], "quantity": row[1], "price": row[2]})
     stock_file.close()
 
-    # stock = [ {"item": row[0], "quan": int(row[1]), "price": int(row[2])} for row in
-    #         [ [cell.strip("'") for cell in line.strip().split(', ')]
-    #         for line in open(filepath, 'r')] ]
+    stock = [ {"item": row[0], "quantity": int(row[1]), "price": int(row[2])}
+            for row in [ [cell.strip("'") for cell in line.strip().split(', ')]
+            for line in open(filepath, 'r')] ]
 
     return stock
 
